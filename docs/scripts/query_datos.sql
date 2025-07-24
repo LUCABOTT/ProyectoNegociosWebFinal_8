8ee82a7330ba2eefab305908c07f831a2e140533
jose9 <jose9@JOSE>
2025-07-24 05:12:13 +0000

    Recuperar y agregar query_datos.sql con script de consulta

---
+++ docs/scripts/query_datos.sql
@@ -0,0 +1,57 @@
+INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (1, 'ECOMMERCE', 'Mouse Gaming', 200, 'https://placehold.co/290x250?text=Negocios-Web-Intro&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (2, 'ECOMMERCE', 'Teclado Gaming', 300, 'https://placehold.co/290x250?text=Negocios-Web-2&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (3, 'Negocios Web Advance', 'Monitor 120hz', 700, 'https://placehold.co/290x250?text=Negocios-Web-Advance&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (4, 'ECOMMERCE', 'Mouse Pad', 1000, 'https://placehold.co/290x250?text=Negocios-Web-Full&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (5, 'ECOMMERCE', 'Laptop OMEN', 1500, 'https://placehold.co/290x250?text=Negocios-Web-Master&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (6, 'ECOMMERCE', 'Laptop ASUS', 2000, 'https://placehold.co/290x250?text=Negocios-Web-Expert&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (7, 'ECOMMERCE', 'PlayStation 5', 2500, 'https://placehold.co/290x250?text=Negocios-Web-Guru&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (8, 'ECOMMERCE', 'XBOX 360', 1500, 'https://placehold.co/290x250?text=Negocios-Web-Master-Ninja&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (9, 'ECOMMERCE', 'XBOX SERIES X', 2000, 'https://placehold.co/290x250?text=Negocios-Web-Expert-Ninja&font=roboto','ACT');
+
+  INSERT INTO `products` (`productId`, `productName`, `productDescription`, `productPrice`, `productImgUrl`, `productStatus`) VALUES
+  (10, 'ECOMMERCE', 'PC Gamer', 2500, 'https://placehold.co/290x250?text=Negocios-Web-Guru-Ninja&font=roboto','ACT');
+
+  INSERT INTO `sales` (`saleId`, `productId`, `salePrice`, `saleStart`, `saleEnd`) VALUES
+  (1, 1, 200, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+  INSERT INTO `sales` (`saleId`, `productId`, `salePrice`, `saleStart`, `saleEnd`) VALUES
+  (2, 3, 700, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+  INSERT INTO `sales` (`saleId`, `productId`, `salePrice`, `saleStart`, `saleEnd`) VALUES
+  (3, 5, 1500, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+    INSERT INTO `sales` (`saleId`, `productId`, `salePrice`, `saleStart`, `saleEnd`) VALUES
+  (4, 7, 2500, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+  INSERT INTO `sales` (`saleId`, `productId`, `salePrice`, `saleStart`, `saleEnd`) VALUES
+  (5, 9, 2000, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+
+  INSERT INTO `highlights` ( `highlightId`, `productId`, `highlightStart`, `highlightEnd`) VALUES
+  (1, 2, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+
+  INSERT INTO `highlights` ( `highlightId`, `productId`, `highlightStart`, `highlightEnd`) VALUES
+  (2, 4, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+    INSERT INTO `highlights` ( `highlightId`, `productId`, `highlightStart`, `highlightEnd`) VALUES
+  (3, 6, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+    INSERT INTO `highlights` ( `highlightId`, `productId`, `highlightStart`, `highlightEnd`) VALUES
+  (4, 8, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
+    INSERT INTO `highlights` ( `highlightId`, `productId`, `highlightStart`, `highlightEnd`) VALUES
+  (5, 10, '2023-08-01 00:00:00', '2023-10-31 23:59:59');
